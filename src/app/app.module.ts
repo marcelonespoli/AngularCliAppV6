366ed68d438ff3d7ad31dc9c55bceba71b218b30
Marcelo <marcelo.santos@abilitysoftware.ie>
2018-08-19 19:30:39 +0100

    changes for layout and added home component and login-menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,33 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// bootstrap
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
+// components
 import { AppComponent } from './app.component';
 import { UpperMenuComponent } from './shared/upper-menu/upper-menu.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MainContentComponent } from './shared/main-content/main-content.component';
+import { HomeComponent } from './home/home.component';
+import { LoginMenuComponent } from './shared/login-menu/login-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UpperMenuComponent,
     FooterComponent,
-    MainContentComponent
+    MainContentComponent,
+    HomeComponent,
+    LoginMenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
